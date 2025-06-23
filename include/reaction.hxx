af3262ab735b826f6b72036d87bcfdc2ba7b989f
Owen Parry <owen.parry@ukaea.uk>
2025-06-23 10:01:03 +0100

    Start Reaction::transform.

--- include/reaction.hxx
+++ include/reaction.hxx
@@ -12,41 +12,14 @@ struct Reaction : public Component {
     static int instance_num{0};
     return instance_num++;
   }
-
-  // Stoich vector
-  std::map<std::string, int> S;
-
-  //   void transform(Options& state) override {
-  //     Options& electron = state["species"]["e"];
-  //     Options& from = state["species"][this->from_species]; // e.g. "h"
-  //     Options& to = state["species"][this->to_species];     // e.g. "h+"
-  //     Field3D reaction_rate, momentum_exchange, energy_exchange, energy_loss;
-
-  //     electron_reaction(electron, from, to, get_rate_coeffs(), get_rad_coeffs(),
-  //                       get_electron_heating(), reaction_rate, momentum_exchange,
-  //                       energy_exchange, energy_loss, this->rate_multiplier,
-  //                       this->radiation_multiplier);
-
-  //     if (this->diagnose) {
-  //       set_diagnostic_fields(reaction_rate, momentum_exchange, energy_exchange,
-  //                             energy_loss);
-  //     }
-  //   }
-
-  /**
-   * @brief Set values in Field3D objects associated with diagnostics.
-   *
-   * @todo Make pure virtual to force implementation?
-   *
-   * @param reaction_rate
-   * @param momentum_exchange
-   * @param energy_exchange
-   * @param energy_loss
-   */
+  void transform(Options& state) override final;
 
 protected:
   /// Normalisations
   BoutReal Tnorm, Nnorm, FreqNorm;
+
+  // Stoich vector
+  std::map<std::string, int> S;
 
   // For diagnostics
   bool diagnose;                                  ///< Outputting diagnostics?
@@ -55,6 +28,9 @@ protected:
   //   Field3D F;                                      ///< Momentum exchange
   //   Field3D E;                                      ///< Energy exchange
   //   Field3D R;                                      ///< Radiation loss
+
+  // Reaction rate coefficients evaluation delegated to subclasses
+  virtual BoutReal eval_rate_coeff(BoutReal T, BoutReal n) = 0;
 
   virtual void set_diagnostic_fields(Field3D& reaction_rate, Field3D& momentum_exchange,
                                      Field3D& energy_exchange, Field3D& energy_loss){};
--- src/reaction.cxx
+++ src/reaction.cxx
@@ -1,7 +1,9 @@
-#include "reaction.hxx"
-
 #include <memory>
 #include <regex>
+
+#include "integrate.hxx"
+
+#include "reaction.hxx"
 
 Reaction::Reaction(std::string name, Options& alloptions) : name(name) {
 
@@ -29,4 +31,41 @@ Reaction::Reaction(std::string name, Options& alloptions) : name(name) {
   }
 
   this->parser = std::make_unique<ReactionParser>(reaction_str);
+}
+
+void Reaction::transform(Options& state) {
+
+  Field3D momentum_exchange, energy_exchange, energy_loss;
+
+  std::vector<std::string> reactant_species = parser->get_reactant_species();
+
+  // Restrict to 2 reactants for now;
+  ASSERT1(reactant_species.size() == 2);
+  Options& r1 = state["species"][reactant_species[0]];
+  Options& r2 = state["species"][reactant_species[1]];
+
+  Field3D n_r1 = get<Field3D>(r1["density"]);
+  Field3D n_r2 = get<Field3D>(r2["density"]);
+  Field3D Te = get<Field3D>(state["species"]["e"]["temperature"]);
+
+  Field3D reaction_rate = cellAverage(
+      [&](BoutReal ne, BoutReal n1, BoutReal te) {
+        return ne * n1 * eval_rate_coeff(te * Tnorm, ne * Nnorm) * Nnorm / FreqNorm
+               * rate_multiplier;
+      },
+      n_r1.getRegion("RGN_NOBNDRY"))(n_r1, n_r2, Te);
+
+  // Use the Stoichiometry 'matrix' (vector) to set density sources for each species
+  for (auto el : parser->get_stoich()) {
+    std::string sp_name = el.first;
+    int weight = el.second;
+    if (weight != 0) {
+      // Density sources
+      add(state["species"][sp_name]["density_source"], weight * reaction_rate);
+    }
+  }
+
+  if (this->diagnose) {
+    set_diagnostic_fields(reaction_rate, momentum_exchange, energy_exchange, energy_loss);
+  }
 }
