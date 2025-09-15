0dd5632865469a55748e8cad2f8c029cce566b54
Owen Parry <owen.parry@ukaea.uk>
2025-09-15 13:45:19 +0100

    First working version of templated RateHelper.

--- include/rate_helper.hxx
+++ include/rate_helper.hxx
@@ -11,8 +11,16 @@
 #include "hermes_build_config.hxx"
 #include "integrate.hxx"
 
+enum class RateParamsTypes { T, ET, nT };
+
+// This is a workaround before CWG2518/P2593R1, taken from cppreference.com
+template <RateParamsTypes>
+constexpr bool dependent_false = false;
+
 typedef std::function<BoutReal(BoutReal, BoutReal, BoutReal)> RateFunctionType;
-template <typename LimiterType = hermes::Limiter, typename IdxType = Ind3D>
+template <RateParamsTypes RateParamsType = RateParamsTypes::nT,
+          typename LimiterType = hermes::Limiter, typename IdxType = Ind3D>
+
 struct RateHelper {
   /**
    * @brief Construct a new RateHelper, extracting and storing some fields from the state
@@ -30,14 +38,33 @@ struct RateHelper {
              RateFunctionType rate_calc_func, const Region<IdxType> region)
       : rate_calc_func(rate_calc_func), region(region) {
 
-    // Extract electron properties from state
-    const Options& electron = state["species"]["e"];
-    n_e = get<Field3D>(electron["density"]);
-    T_e = get<Field3D>(electron["temperature"]);
+    // Electron temperature is always a rate parameter
+    std::vector<std::string> rate_param_names = {"e:temperature"};
+
+    if constexpr (RateParamsType == RateParamsTypes::ET) {
+      static_assert(dependent_false<RateParamsType>,
+                    "RateParamsTypes::ET not implemented");
+    } else if constexpr (RateParamsType == RateParamsTypes::nT) {
+      rate_param_names.push_back("e:density");
+    } else if constexpr (RateParamsType == RateParamsTypes::T) {
+      // pass
+    } else {
+      // Compile-time error if any other RateParamsType enum exists
+      static_assert(dependent_false<RateParamsType>, "Unhandled RateParamsType");
+    }
+
+    // Extract and store rate param fields
+    std::transform(rate_param_names.begin(), rate_param_names.end(),
+                   std::inserter(this->rate_params, this->rate_params.end()),
+                   [&](const std::string& rate_param_name) {
+                     return std::make_pair(
+                         rate_param_name,
+                         get<Field3D>(state["species"][rate_param_name]));
+                   });
 
     // Extract and store reactant densities
     std::transform(reactant_names.begin(), reactant_names.end(),
-                   std::back_inserter(n_reactants),
+                   std::back_inserter(this->n_reactants),
                    [&](const std::string& reactant_name) {
                      return get<Field3D>(state["species"][reactant_name]["density"]);
                    });
@@ -52,7 +79,7 @@ struct RateHelper {
    * @return Field3D the cell-averaged reaction rate
    */
   Field3D calc_rate() {
-    Field3D reaction_rate{emptyFrom(n_e)};
+    Field3D reaction_rate{emptyFrom(this->rate_params["e:temperature"])};
     auto J = reaction_rate.getCoordinates()->J;
     BOUT_FOR(i, region) {
 
@@ -60,18 +87,76 @@ struct RateHelper {
       auto ym = i.ym();
       auto Ji = J[i];
 
-      reaction_rate[i] =
-          4. / 6 * rate_calc_func(mass_action(i), n_e[i], T_e[i])
-          + (Ji + J[ym]) / (12. * Ji)
-                * rate_calc_func(mass_action_left(i, ym, yp),
-                                 cellLeft<LimiterType>(n_e[i], n_e[ym], n_e[yp]),
-                                 cellLeft<LimiterType>(T_e[i], T_e[ym], T_e[yp]))
-          + (Ji + J[yp]) / (12. * Ji)
-                * rate_calc_func(mass_action_right(i, ym, yp),
-                                 cellRight<LimiterType>(n_e[i], n_e[ym], n_e[yp]),
-                                 cellRight<LimiterType>(T_e[i], T_e[ym], T_e[yp]));
+      // Calc rate_central, rate_left, rate_right at this index
+      BoutReal rate_central, rate_left, rate_right;
+      if constexpr (RateParamsType == RateParamsTypes::T) {
+        rate_central = rate_calc_func(mass_action(i), get_rate_param("e:temperature", i));
+        rate_left = rate_calc_func(mass_action_left(i, ym, yp),
+                                   get_rate_param_left("e:temperature", i, ym, yp));
+        rate_right = rate_calc_func(mass_action_right(i, ym, yp),
+                                    get_rate_param_right("e:temperature", i, ym, yp));
+      } else if constexpr (RateParamsType == RateParamsTypes::ET) {
+        static_assert(dependent_false<RateParamsType>,
+                      "RateParamsTypes::ET rate not implemented");
+      } else if constexpr (RateParamsType == RateParamsTypes::nT) {
+        rate_central = rate_calc_func(mass_action(i), get_rate_param("e:density", i),
+                                      get_rate_param("e:temperature", i));
+        rate_left = rate_calc_func(mass_action_left(i, ym, yp),
+                                   get_rate_param_left("e:density", i, ym, yp),
+                                   get_rate_param_left("e:temperature", i, ym, yp));
+        rate_right = rate_calc_func(mass_action_right(i, ym, yp),
+                                    get_rate_param_right("e:density", i, ym, yp),
+                                    get_rate_param_right("e:temperature", i, ym, yp));
+      } else {
+        // Compile-time error if any other RateParamsType enum exists
+        static_assert(dependent_false<RateParamsType>, "Unhandled RateParamsType");
+      }
+
+      // Overall rate at this index
+      reaction_rate[i] = 4. / 6 * rate_central + (Ji + J[ym]) / (12. * Ji) * rate_left
+                         + (Ji + J[yp]) / (12. * Ji) * rate_right;
     }
     return reaction_rate;
+  }
+
+  /**
+   * @brief Extract the (cell-centre) value of a rate parameter.
+   *
+   * @param name name of the parameter (label in state["species"])
+   * @param i central index
+   * @return BoutReal
+   */
+  BoutReal get_rate_param(const std::string& name, IdxType i) {
+    return this->rate_params[name][i];
+  }
+
+  /**
+   * @brief Extract the value of a rate parameter at the left of a cell.
+   *
+   * @param name name of the parameter (label in state["species"])
+   * @param i central index
+   * @param ym neighbour 1 index
+   * @param yp neighbour 2 index
+   * @return BoutReal
+   */
+  BoutReal get_rate_param_left(const std::string& name, IdxType i, IdxType ym,
+                               IdxType yp) {
+    return cellLeft<LimiterType>(this->rate_params[name][i], this->rate_params[name][ym],
+                                 this->rate_params[name][yp]);
+  }
+
+  /**
+   * @brief Extract the value of a rate parameter at the right of a cell.
+   * @param name name of the parameter (label in state["species"])
+   * @param i central index
+   * @param ym neighbour 1 index
+   * @param yp neighbour 2 index
+   * @return BoutReal
+   */
+  BoutReal get_rate_param_right(const std::string& name, IdxType i, IdxType ym,
+                                IdxType yp) {
+    return cellRight<LimiterType>(this->rate_params[name][i], this->rate_params[name][ym],
+                                  this->rate_params[name][yp]);
   }
 
 private:
@@ -79,11 +164,12 @@ private:
   const Region<IdxType> region;
   /// Function to calculate reaction rate as a function of n_e, T_e
   RateFunctionType rate_calc_func;
-  /// Electron density and temperature
-  Field3D n_e;
-  Field3D T_e;
+
   /// Reactant densities
   std::vector<Field3D> n_reactants;
+
+  // Rate parameter fields
+  std::map<std::string, Field3D> rate_params;
 
   BoutReal mass_action(IdxType i) {
     BoutReal result = 1;
