5586f508509825e8ea2b406a589fd7a698c69954
Ben Dudson <dudson2@llnl.gov>
2025-10-29 14:30:00 -0700

    electromagnetic: Add unit tests
    Check that Electromagnetic::transform runs without charged species,
    setting the Apar and Apar_flutter fields to zero.

--- src/electromagnetic.cxx
+++ src/electromagnetic.cxx
@@ -32,7 +32,10 @@ Electromagnetic::Electromagnetic(std::string name, Options &alloptions, Solver* 
   // Use the "Naulin" solver because we need to include toroidal
   // variations of the density (A coefficient)
   aparSolver = Laplacian::create(&options["laplacian"]);
-  aparSolver->savePerformance(*solver, "AparSolver");
+  if (solver != nullptr) {
+    // solver may be null in unit testing
+    aparSolver->savePerformance(*solver, "AparSolver");
+  }
 
   const_gradient = options["const_gradient"]
     .doc("Extrapolate gradient of Apar into all radial boundaries?")
