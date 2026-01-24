ff50561f29d5bd1908229266ec828ff7ea93ff28
Ben Dudson <dudson2@llnl.gov>
2026-01-23 17:19:16 -0800

    relax_potential: Fix for MXG = 1
    If MXG < 2 then don't fill the additional guard cell.

--- src/relax_potential.cxx
+++ src/relax_potential.cxx
@@ -425,11 +425,14 @@ void RelaxPotential::transform_impl(GuardedOptions& state) {
         // Set midpoint (boundary) value
         for (int k = 0; k < mesh->LocalNz; k++) {
           phi(mesh->xstart - 1, j, k) = 2. * phivalue - phi(mesh->xstart, j, k);
-
-          // Note: This seems to make a difference, but don't know why.
-          // Without this, get convergence failures with no apparent instability
-          // (all fields apparently smooth, well behaved)
-          phi(mesh->xstart - 2, j, k) = phi(mesh->xstart - 1, j, k);
+        }
+        if (mesh->xstart > 1) {
+          for (int k = 0; k < mesh->LocalNz; k++) {
+            // Note: This seems to make a difference, but don't know why.
+            // Without this, get convergence failures with no apparent instability
+            // (all fields apparently smooth, well behaved)
+            phi(mesh->xstart - 2, j, k) = phi(mesh->xstart - 1, j, k);
+          }
         }
       }
     }
@@ -445,11 +448,14 @@ void RelaxPotential::transform_impl(GuardedOptions& state) {
         // Set midpoint (boundary) value
         for (int k = 0; k < mesh->LocalNz; k++) {
           phi(mesh->xend + 1, j, k) = 2. * phivalue - phi(mesh->xend, j, k);
-
-          // Note: This seems to make a difference, but don't know why.
-          // Without this, get convergence failures with no apparent instability
-          // (all fields apparently smooth, well behaved)
-          phi(mesh->xend + 2, j, k) = phi(mesh->xend + 1, j, k);
+        }
+        if (mesh->xend < mesh->LocalNx - 2) {
+          for (int k = 0; k < mesh->LocalNz; k++) {
+            // Note: This seems to make a difference, but don't know why.
+            // Without this, get convergence failures with no apparent instability
+            // (all fields apparently smooth, well behaved)
+            phi(mesh->xend + 2, j, k) = phi(mesh->xend + 1, j, k);
+          }
         }
       }
     }
