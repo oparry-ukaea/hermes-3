830471db40e1ea93f6489a8322fe74139686912c
Ben Dudson <dudson2@llnl.gov>
2026-06-26 11:58:03 -0700

    electromagnetic: Formatting

--- src/electromagnetic.cxx
+++ src/electromagnetic.cxx
@@ -1,8 +1,8 @@
 #include "../include/electromagnetic.hxx"
 
 #include <bout/constants.hxx>
-#include <bout/mesh.hxx>
 #include <bout/invert_laplace.hxx>
+#include <bout/mesh.hxx>
 
 // Set the default acceptance tolerances for the Naulin solver.
 // These are used if the maximum iterations is reached.
@@ -43,8 +43,8 @@ Electromagnetic::Electromagnetic(std::string name, Options& alloptions, Solver* 
   }
 
   const_gradient = options["const_gradient"]
-    .doc("Extrapolate gradient of Apar into all radial boundaries?")
-    .withDefault<bool>(false);
+                       .doc("Extrapolate gradient of Apar into all radial boundaries?")
+                       .withDefault<bool>(false);
 
   // Give Apar an initial value because we solve Apar by iteration
   // starting from the previous solution
@@ -58,35 +58,35 @@ Electromagnetic::Electromagnetic(std::string name, Options& alloptions, Solver* 
     last_time = 0.0;
 
     apar_boundary_timescale = options["apar_boundary_timescale"]
-      .doc("Timescale for Apar boundary relaxation [seconds]")
-      .withDefault(1e-8)
-      / get<BoutReal>(alloptions["units"]["seconds"]);
+                                  .doc("Timescale for Apar boundary relaxation [seconds]")
+                                  .withDefault(1e-8)
+                              / get<BoutReal>(alloptions["units"]["seconds"]);
 
   } else if (options["apar_boundary_neumann"]
-      .doc("Neumann on all radial boundaries?")
-      .withDefault<bool>(false)) {
+                 .doc("Neumann on all radial boundaries?")
+                 .withDefault<bool>(false)) {
     // Set zero-gradient (neumann) boundary condition DC on the core
     aparSolver->setInnerBoundaryFlags(INVERT_DC_GRAD + INVERT_AC_GRAD);
     aparSolver->setOuterBoundaryFlags(INVERT_DC_GRAD + INVERT_AC_GRAD);
 
   } else if (options["apar_core_neumann"]
-      .doc("Neumann radial boundary in the core? False => Dirichlet")
-        .withDefault<bool>(true)
+                 .doc("Neumann radial boundary in the core? False => Dirichlet")
+                 .withDefault<bool>(true)
              and bout::globals::mesh->periodicY(bout::globals::mesh->xstart)) {
     // Set zero-gradient (neumann) boundary condition DC on the core
     aparSolver->setInnerBoundaryFlags(INVERT_DC_GRAD);
   }
 
-  diagnose = options["diagnose"]
-    .doc("Output additional diagnostics?")
-    .withDefault<bool>(false);
+  diagnose =
+      options["diagnose"].doc("Output additional diagnostics?").withDefault<bool>(false);
 
   magnetic_flutter = options["magnetic_flutter"]
-    .doc("Set magnetic flutter terms (Apar_flutter)?")
-    .withDefault<bool>(false);
+                         .doc("Set magnetic flutter terms (Apar_flutter)?")
+                         .withDefault<bool>(false);
 
-  if (magnetic_flutter)
+  if (magnetic_flutter) {
     setPermissions(readWrite("fields:Apar_flutter"));
+  }
 }
 
 void Electromagnetic::restartVars(Options& state) {
@@ -98,7 +98,8 @@ void Electromagnetic::restartVars(Options& state) {
   if (first and state.isSet("Apar")) {
     first = false;
     Apar = state["Apar"].as<Field3D>();
-    output.write("\nElectromagnetic: Read Apar from restart file (min {}, max {})\n", min(Apar), max(Apar));
+    output.write("\nElectromagnetic: Read Apar from restart file (min {}, max {})\n",
+                 min(Apar), max(Apar));
   }
 
   // Save the Apar field. It is solved using an iterative method,
@@ -163,9 +164,9 @@ void Electromagnetic::transform_impl(GuardedOptions& state) {
       const int x = mesh->xstart - 1;
       for (int y = mesh->ystart; y <= mesh->yend; y++) {
         for (int z = mesh->zstart; z <= mesh->zend; z++) {
-          rhs(x, y, z) = (weight * (Apar(x + 1, y, z) - Apar(x, y, z)) +
-                          (1 - weight) * (Apar(x + 2, y, z) - Apar(x + 1, y, z))) /
-            (sqrt(coords->g_11(x, y)) * coords->dx(x, y));
+          rhs(x, y, z) = (weight * (Apar(x + 1, y, z) - Apar(x, y, z))
+                          + (1 - weight) * (Apar(x + 2, y, z) - Apar(x + 1, y, z)))
+                         / (sqrt(coords->g_11(x, y)) * coords->dx(x, y));
         }
       }
     }
@@ -173,9 +174,9 @@ void Electromagnetic::transform_impl(GuardedOptions& state) {
       const int x = mesh->xend + 1;
       for (int y = mesh->ystart; y <= mesh->yend; y++) {
         for (int z = mesh->zstart; z <= mesh->zend; z++) {
-          rhs(x, y, z) =  (weight * (Apar(x, y, z) - Apar(x - 1, y, z)) +
-                           (1 - weight) * (Apar(x - 1, y, z) - Apar(x - 2, y, z))) /
-            sqrt(coords->g_11(x, y)) / coords->dx(x, y);
+          rhs(x, y, z) = (weight * (Apar(x, y, z) - Apar(x - 1, y, z))
+                          + (1 - weight) * (Apar(x - 1, y, z) - Apar(x - 2, y, z)))
+                         / sqrt(coords->g_11(x, y)) / coords->dx(x, y);
         }
       }
     }
@@ -247,40 +248,39 @@ void Electromagnetic::transform_impl(GuardedOptions& state) {
   }
 }
 
-void Electromagnetic::outputVars(Options &state) {
+void Electromagnetic::outputVars(Options& state) {
   // Normalisations
   auto Bnorm = get<BoutReal>(state["Bnorm"]);
   auto rho_s0 = get<BoutReal>(state["rho_s0"]);
 
-  set_with_attrs(state["beta_em"], beta_em, {
-     {"long_name", "Helmholtz equation parameter"}
-   });
+  set_with_attrs(state["beta_em"], beta_em,
+                 {{"long_name", "Helmholtz equation parameter"}});
 
-  set_with_attrs(state["Apar"], Apar, {
-      {"time_dimension", "t"},
-      {"units", "T m"},
-      {"conversion", Bnorm * rho_s0},
-      {"standard_name", "b dot A"},
-      {"long_name", "Parallel component of vector potential A"}
-    });
+  set_with_attrs(state["Apar"], Apar,
+                 {{"time_dimension", "t"},
+                  {"units", "T m"},
+                  {"conversion", Bnorm * rho_s0},
+                  {"standard_name", "b dot A"},
+                  {"long_name", "Parallel component of vector potential A"}});
 
   if (magnetic_flutter) {
-    set_with_attrs(state["Apar_flutter"], Apar_flutter, {
-      {"time_dimension", "t"},
-      {"units", "T m"},
-      {"conversion", Bnorm * rho_s0},
-      {"standard_name", "b dot A"},
-      {"long_name", "Vector potential A|| used in flutter terms"}
-    });
+    set_with_attrs(state["Apar_flutter"], Apar_flutter,
+                   {{"time_dimension", "t"},
+                    {"units", "T m"},
+                    {"conversion", Bnorm * rho_s0},
+                    {"standard_name", "b dot A"},
+                    {"long_name", "Vector potential A|| used in flutter terms"}});
   }
 
   if (diagnose) {
-    set_with_attrs(state["Ajpar"], Ajpar, {
-      {"time_dimension", "t"},
-    });
+    set_with_attrs(state["Ajpar"], Ajpar,
+                   {
+                       {"time_dimension", "t"},
+                   });
 
-    set_with_attrs(state["alpha_em"], alpha_em, {
-      {"time_dimension", "t"},
-    });
+    set_with_attrs(state["alpha_em"], alpha_em,
+                   {
+                       {"time_dimension", "t"},
+                   });
   }
 }
